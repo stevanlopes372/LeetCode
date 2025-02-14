ab4961e4c47e7590e5c8352424cbbd5e1067f65c
Stevan Santiago Lopes <63737436+stevanlopes372@users.noreply.github.com>
2025-02-14 07:08:49 -0300

    Time: 439 ms (38.53%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,25 +1,5 @@
-# Write your MySQL query statement below
-with a as (
-select
-    machine_id,
-    activity_type,
-    timestamp as start_time,
-    lead(timestamp) over(partition by machine_id, process_id order by timestamp) as end_time
-from Activity
-),
-
-b as (
-select
-    machine_id,
-    end_time - start_time as diff
-from
-    a
-where
-    activity_type = 'start'
-)
-
-select distinct
-    machine_id,
-    round(avg(diff) over (partition by machine_id), 3) as processing_time
-from
-    b
+Select a.machine_id , ROUND(avg(b.timestamp - a.timestamp),3) as processing_time
+FROM Activity as a
+JOIN Activity as b
+ON a.machine_id = b.machine_id and a.process_id = b.process_id and a.activity_type = 'start' and b.activity_type = 'end'
+GROUP BY a.machine_id;

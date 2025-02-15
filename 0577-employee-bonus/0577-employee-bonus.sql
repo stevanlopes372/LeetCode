c1dbaf7bd13e8c0ccb81c62217e0012b3faff8d8
Stevan Santiago Lopes <63737436+stevanlopes372@users.noreply.github.com>
2025-02-15 14:27:46 -0300

    Time: 1975 ms (31.97%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
-with a as(
 select
-    e.empId,
     e.name,
     b.bonus
 from Employee e
@@ -9,7 +7,3 @@ left join
     Bonus b
     using(empId)
 where empId not in (select empId from Bonus where bonus >= 1000)
-order by
-    empId
-)
-select name, bonus from a

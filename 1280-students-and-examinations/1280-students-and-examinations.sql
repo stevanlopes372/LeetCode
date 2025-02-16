55c6b1e5eec1dedde2c62ca3e301847067289840
Stevan Santiago Lopes <63737436+stevanlopes372@users.noreply.github.com>
2025-02-16 09:47:55 -0300

    Time: 1696 ms (44.74%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,25 +1,14 @@
-# Write your MySQL query statement below
-with e as (
-    select
-        concat(student_id,subject_name) as concatenade
-    from
-        Examinations
-)
-select
-    s.student_id,
-    s.student_name,
-    su.subject_name,
-    count(e.concatenade) as attended_exams
-from
-    Students s
-cross join
-    Subjects su
-left join e
-    on concat(s.student_id, su.subject_name) = e.concatenade
-group by
-    s.student_id,
-    s.student_name,
-    su.subject_name
-order by
-    s.student_id,
-    su.subject_name
+SELECT
+    S.student_id
+    ,S.student_name
+    ,SU.subject_name
+    ,COUNT(E.student_id) attended_exams
+FROM Students S
+CROSS JOIN Subjects SU
+LEFT JOIN Examinations E
+    ON S.student_id = E.student_id
+    AND SU.subject_name = E.subject_name
+
+GROUP BY S.student_id, S.student_name, SU.subject_name
+ORDER BY S.student_id, S.student_name, SU.subject_name
+;
